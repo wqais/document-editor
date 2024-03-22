7a14d1789955b9de78ed01124fe11e904090ab3c
Qais Warekar <warekarqais@gmail.com>
2024-03-22 16:56:34 +0530

    Remote server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // Replace with your backend URL
+const socket = io('https://document-backend.vercel.app/'); // Replace with your backend URL
 
 function App() {
   const [content, setContent] = useState('');
